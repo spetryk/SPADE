411b50d3c23dab4847a4f78d8c3f68622411a989
Suzie Petryk <spetryk@berkeley.edu>
2019-10-29 11:50:25 -0700

    ray version, throws connection error 'Protocol wrong type for socket'

--- test.py
+++ test.py
@@ -12,6 +12,13 @@ from models.pix2pix_model import Pix2PixModel
 from util.visualizer import Visualizer
 from util import html
 
+import ray
+import psutil
+
+num_cpus = psutil.cpu_count(logical=False)
+
+ray.init(num_cpus=num_cpus, object_store_memory=9e9)
+
 opt = TestOptions().parse()
 
 dataloader = data.create_dataloader(opt)
@@ -29,17 +36,26 @@ webpage = html.HTML(web_dir,
                     (opt.name, opt.phase, opt.which_epoch))
 
 # test
+
+@ray.remote
+def run(data_i):
+    print("generating image..")
+    generated = model(data_i, mode='inference')
+    return generated
+
 for i, data_i in enumerate(dataloader):
     if i * opt.batchSize >= opt.how_many:
         break
 
-    generated = model(data_i, mode='inference')
-
+    generated_id = run.remote(data_i)
+    generated = ray.get(generated_id)
     img_path = data_i['path']
+
     for b in range(generated.shape[0]):
         print('process image... %s' % img_path[b])
         visuals = OrderedDict([('input_label', data_i['label'][b]),
                                ('synthesized_image', generated[b])])
         visualizer.save_images(webpage, visuals, img_path[b:b + 1])
 
-webpage.save()
+
+webpage.save.remote()
